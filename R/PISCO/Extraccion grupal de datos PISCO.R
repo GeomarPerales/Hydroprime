fd6f531f3dca71228365d8c2adb69264726ef535
Geomar Perales Apaico <per3app@gmail.com>
2022-01-26 19:40:50 -0500

    Add files via upload

--- R/PISCO/Extraccion grupal de datos PISCO.R
+++ R/PISCO/Extraccion grupal de datos PISCO.R
@@ -1,28 +1,14 @@
 #Para un grupo de estaciones
-library(openxlsx)
 library(devtools)
 install_github("GeomarPerales/RPisco")
 library(RPisco)
+library(openxlsx)
 
-##########################################################################
 # DIARIO
-# netCDF de la variable
-PISCO.file <- "D:\\PISCOd_pp_2.1.nc" 
-data <- read.xlsx("estaciones.xlsx") 
-x <- data.frame(PISCO.file, data)
-data <- piscodgroup(x)
-
-# exportación a excel
-write.xlsx(data,"D:\\pp_diaria.xlsx")
-
-##########################################################################
-# MENSUAL
-# netCDF de la variable
-PISCO.file <- "D:\\PISCOm_pp_2.1.nc" 
+PISCO.file <- "D:\\PISCOd_pp_2.1.nc"# netCDF de la variable
 data <- read.xlsx("estaciones.xlsx") 
 x <- data.frame(PISCO.file, data)
 data <- piscomgroup(x)
 
 # exportación a excel
-write.xlsx(data,"D:\\pp_mensual.xlsx")
-
+write.xlsx(data,"D:\\pp_grupo_mensual.xlsx")
--- R/PISCO/Extraccion individual de datos PISCO.R
+++ R/PISCO/Extraccion individual de datos PISCO.R
@@ -1,27 +1,13 @@
 #Para una estación
-library(openxlsx)
 library(devtools)
 install_github("GeomarPerales/RPisco")
 library(RPisco)
+library(openxlsx)
 
-##########################################################################
-# DIARIO
-# netCDF de la variable
-PISCO.file <- "D:\\PISCOd_pp_2.1.nc" 
-x <- data.frame(PISCO.file, -74.2258, -13.1605)
-data <- piscod(x)
-
-# exportación a excel
-write.xlsx(data,"D:\\pp_diaria.xlsx") 
-
-
-##########################################################################
 # MENSUAL
-# netCDF de la variable
-PISCO.file <- "D:\\PISCOm_pp_2.1.nc" 
+PISCO.file <- "D:\\PISCOm_pp_2.1.nc"  # netCDF de la variable
 x <- data.frame(PISCO.file, -74.2258, -13.1605)
 data <- piscom(x)
 
 # exportación a excel
-write.xlsx(data,"D:\\pp_mensual.xlsx")
-
+write.xlsx(data,"D:\\pp_mensual.xlsx") 
